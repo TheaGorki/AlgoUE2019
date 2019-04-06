0af15a4b1a746a0fbd080effada8d2933a0347b1
Thea Mac <Thea@Theas-MacBook-Air.local>
2019-04-06 18:41:51 +0200

    add #A4

---
+++ A4/TheaGorki-ManhattanTouristDHV.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+
+def ManhattanTourist(n, m, down_matrix, right_matrix, diagonal_matrix):
+    '''
+    Solve Manhattan Tourist Problem via dynamic programming (including diagonal)
+    '''
+
+    mht_dic = {(0,0):0}
+
+    for i in range(1, n):
+        mht_dic[(i,0)] = mht_dic[(i-1,0)] + down_matrix[i-1][0]
+
+    for j in range(1, m):
+        mht_dic[(0,j)] = mht_dic[(0,j-1)] + right_matrix[0][j-1]
+
+    # calulating
+    for i in range(1, n):
+        for j in range(1,m):
+            mht_dic[(i,j)] = max(mht_dic[(i-1,j)]+down_matrix[i-1][j], mht_dic[(i,j-1)] + right_matrix[i][j-1], mht_dic[(i-1,j-1)] + diagonal_matrix[i-1][j-1])
+
+    return mht_dic[(n-1,m-1)]
+
+if __name__ == "__main__":
+    # Read the input data
+    import sys
+    data = sys.stdin
+
+    line=data.readline().rstrip('\n')
+    down_matrix= []
+    while (line != '---'):
+            if line.startswith('G'):
+                    line = data.readline().rstrip('\n')
+            else:
+                    down_matrix.append([float(i) for i in line.split('   ')])
+                    line=data.readline().rstrip('\n')
+    if line == '---':
+            line = data.readline().rstrip('\n')
+
+    right_matrix= []
+    while (line != '---'):
+            if line.startswith('G'):
+                    line = data.readline().rstrip('\n')
+            else:
+                    right_matrix.append([float(i) for i in line.split('   ')])
+                    line=data.readline().rstrip('\n')
+    if line == '---':
+        line = data.readline().rstrip('\n')
+
+    diagonal_matrix= []
+    while (line != '---'):
+        if line.startswith('G'):
+            line = data.readline().rstrip('\n')
+        else:
+            diagonal_matrix.append([float(i) for i in line.split('   ')])
+            line = data.readline().rstrip('\n')
+
+    n= int(len(down_matrix[0]))
+    m= int(len(right_matrix))
+    d= int(len(diagonal_matrix))
+
+    if n != m or len(down_matrix) != len(right_matrix[0]) or n | m != len(diagonal_matrix)+1:
+        print("Fehler bei Einlesen von Datei")
+    else:
+        max_dist = ManhattanTourist(n,m,down_matrix,right_matrix, diagonal_matrix)
+        print (max_dist)
